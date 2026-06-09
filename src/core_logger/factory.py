73d193034255b21a9a38eb20f8049ba704ad1fde
elly-zietz <ellyzietz17@gmail.com>
2026-06-08 18:31:21 -0600

    Revert "Add  tier log handler routing"

--- src/core_logger/factory.py
+++ src/core_logger/factory.py
@@ -4,7 +4,6 @@ Logger factory for creating service-bound logger instances.
 Provides get_logger() as the primary entry point for obtaining a Logger
 configured for a specific service name.
 # Need to add log_handler
-# Need to add log_handler
 """
 from .logger import Logger
 
